530016f105aff81e58277b2bf99889b9014e59e5
Oskar Taubert <oskar-t@gmx.de>
2024-08-11 23:23:55 +0200

    simple text buffer as vec of Strings, adding lines from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    // for now the text buffer is a list of lines, each line is a string
+    let mut buffer: Vec<String> = Vec::new();
+
+    loop {
+        let mut new_line = String::new();
+        io::stdin()
+            .read_line(&mut new_line)
+            .expect("Failed to read input");
+        buffer.push(new_line);
+        for line in &buffer {
+            let display_line = &line[0..line.len() - 1];
+            println!("{display_line}");
+        }
+    }
 }
